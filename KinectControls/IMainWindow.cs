a3e77c544dcece810addd3a843c2cd99372684af
Denny <nearffxx@bitbucket.org>
2015-01-18 18:00:32 +0100

    no message

---
+++ KinectControls/IMainWindow.cs
@@ -0,0 +1,9 @@
+﻿using System;
+namespace KinectControls
+{
+    public interface IMainWindow
+    {
+        void setButtonsBackground(string btn0URL);
+        void setButtonsBackground(string btn0URL, string btn1URL, string btn2URL);
+    }
+}
